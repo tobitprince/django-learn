911864d198a2a6d8f58d8c3296bdb6d1e5ed4897
tobitprince <prince.tobit@strathmore.edu>
2024-01-11 11:30:36 +0300

    One to One relationships

--- store/models.py
+++ store/models.py
@@ -30,14 +30,22 @@ class Customer(models.Model):
     membership = models.CharField(max_length = 255, choices = MEMBERSHIP_CHOICES, default = 'MEMBERSHIP_BRONZE')
 
 #Order model
-    class Order(models.Model):
-        PAYMENT_STATUS_PENDING = 'P'
-        PAYMENT_STATUS_COMPLETE = 'C'
-        PAYMENT_STATUS_FAILED = 'F'
-        PAYMENT_STATUS = [
-            ('PAYMENT_STATUS_PENDING', 'Pending'),
-            ('PAYMENT_STATUS_COMPLETE', 'Complete'),
-            ('PAYMENT_STATUS_FAILED', 'Failed'),
-        ]
-        placed_at = models.DateTimeField(auto_now_add = True)
-        payment_status = models.CharField(max_length = 1, choices = PAYMENT_STATUS, default = 'PAYMENT_STATUS_PENDING')
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS = [
+        ('PAYMENT_STATUS_PENDING', 'Pending'),
+        ('PAYMENT_STATUS_COMPLETE', 'Complete'),
+        ('PAYMENT_STATUS_FAILED', 'Failed'),
+    ]
+    placed_at = models.DateTimeField(auto_now_add = True)
+    payment_status = models.CharField(max_length = 1, choices = PAYMENT_STATUS, default = 'PAYMENT_STATUS_PENDING')
+
+class Address(models.Model):
+    street = models.CharField(max_length = 255)
+    city = models.CharField(max_length = 255)
+    customer = models.OneToOneField(Customer, on_delete = models.CASCADE, primary_key = True)
+    
+
+
