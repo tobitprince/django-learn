627cc058df38f198e27f9a7037cb6190f130be17
tobitprince <prince.tobit@strathmore.edu>
2024-01-11 11:06:09 +0300

    Added Product and Customer models to store

--- store/models.py
+++ store/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length = 255)
+    description = models.TextField()
+    ###999.99
+    price = models.DecimalField(max_digits = 6, decimal_places = 2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now = True)
+
+#customer model
+class Customer(models.Model):
+    first_name = models.CharField(max_length = 255)
+    last_name = models.CharField(max_length = 255)
+    email = models.EmailField(unique = True)
+    phone = models.CharField(max_length = 255)
+    birth_date = models.DateField(null = True)
