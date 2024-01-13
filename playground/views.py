3b8e59b89290272a6b767a1456efc65539df0560
tobitprince <prince.tobit@strathmore.edu>
2024-01-13 10:10:44 +0300

    Refactor templates and views, add TaggedItemManager

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
+from django.db.models import Q, F, Func, Value, ExpressionWrapper, DecimalField
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
+from django.db.models.functions import Concat
+from django.contrib.contenttypes.models import ContentType
 from store.models import Product
+from store.models import Customer
+from store.models import Collection
+from store.models import Order
+from store.models import OrderItem
+from tags.models import TaggedItem
 
 # Create your views here.
 
 def say_hello(request):
+    #result = Product.objects.filter(collection__id=3).aggregate(min=Min('unit_price'), max=Max('unit_price'), avg=Avg('unit_price'))
     
-    return render(request, 'hello.html', {'name': 'Mosh'}) 
+    #concat
+    #result = Customer.objects.annotate(
+    #    full_name = Func(F('first_name'), Value(' '), F('last_name'), function = ('CONCAT'))
+    #)
+    #result = Customer.objects.annotate(
+    #    full_name = Concat('first_name', Value(' '), 'last_name')
+    #)
+
+    #result = Customer.objects.annotate(orders_count = Count('order'))
+
+
+    #discounted_price = ExpressionWrapper(F('unit_price') * 0.8, output_field = DecimalField())
+    #result = Product.objects.annotate(discounted_price = discounted_price)
+
+    content_type = ContentType.objects.get_for_model(Product)
+    queryset = TaggedItem.objects.select_related('tag').filter(content_type = content_type, object_id = 1)
+
+    return render(request, 'hello.html', {'name': 'Mosh', 'tags': list(queryset)}) 
